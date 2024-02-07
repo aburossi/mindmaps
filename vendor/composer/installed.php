7cf4b0bcd1174d9f1e0b347dcf5a7a0d18494363
aburossi <pietro.rossi@bbw.ch>
2024-02-07 10:09:53 +0100

    1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '981076e6550578fd637d2d8a5ce6e4728d2024b4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '981076e6550578fd637d2d8a5ce6e4728d2024b4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'samdark/sitemap' => array(
+            'pretty_version' => '2.4.1',
+            'version' => '2.4.1.0',
+            'reference' => 'cf514750781275ad90fc9a828b4330c9c5ccba98',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../samdark/sitemap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
